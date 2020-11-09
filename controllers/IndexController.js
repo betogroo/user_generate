fa5bcbc6663c5f17984ad09fbbaddcaa63a5e01e
betogroo <luizhumberto@gmail.com>
2020-11-09 11:09:44 -0300

    instructions

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -7,7 +7,51 @@ class IndexController {
 
   
 async index(req, res){
-    res.send('app.dev.br')
+    res.json(
+        {  
+            "title": "API de geração de Usuários",
+            "versão": '1.0.0',
+            "Método": 'GET',
+            "Retorno": "Status 200: Array de usuários gerados aleatoriamente (mínimo de 1, máximo de 100",
+            "Dados Retornados": {
+                "id": "UUID",
+                "name": "Nome de pessoa aleatória, em caixa alta",
+                "rg": "Rg de pessoa aleatório, com 8 dígitos",
+                "cpf": "CPF Válido, gerado aleatoriamente",
+                "password": "Password criptografado, onde o valor real é o RG gerado.",
+                "email": "Email, com nome + um sinal separador + sobrenome + número aleatório",
+                "birthDate": "Data de nascimento válida, aleatória, de 01/01/1936 a 31/12/2003",
+                "gender": "Gênero, (M ou F)"
+            },
+            "url": "https://user-generate.herokuapp.com/user-generate/{quantidade}",
+            "parâmetro": [
+                {
+                    "nome": 'quantidade', "tipo": 'Inteiro', 'requerido': 'Não', 
+                    "obs": [
+                            'Se não for passado parâmetro a quantidade padrão de nomes gerados é 10 (dez)',
+                            'Se o parâmetro não for um número a quantidade gerada será 10 (dez)',
+                            'Se o parâmetro foi negativo, a quantidade gerada será 10 (10)',
+                            'Se o parâmetro for número decimal, será considerado apenas o inteiro',
+                            'Se o parâmetro for maior que 100, serão gerados apenas 100 registros',
+                            'O primeiro registro é gerado com dados estáticos'
+                            
+                    ]
+                }
+            ],
+            "Primeiro Registro": 
+                {
+                                id: 'superAdm',
+                                name: 'ADMINISTRADOR DO SISTEMA',
+                                rg: '000000000',
+                                cpf: '00000000000',
+                                password: 'admin',
+                                email: 'admin@api.deb.br',
+                                birthDate: '2000/01/01',
+                                gender: 'M'}
+            
+            
+        }
+)
 }
 }
 
--- modules/UserGenerate/UserGenerateController.js
+++ modules/UserGenerate/UserGenerateController.js
@@ -8,12 +8,29 @@ const {  getRandomPeople } = require ('../../helpers/generates')
 
 class LogController {
 
-    index(req, res){
+    async index(req, res){
         var { amount } = req.params
-        if (!amount || amount === undefined) {
+        if (!amount || amount === undefined || isNaN(amount)) {
             amount = 10
         }
+        if (amount > 100) {
+            amount = 100
+        }
+        if (amount < 1) {
+            amount = 10
+        }
+        amount = Math.trunc(amount)
         const data = []
+            data[0] = {
+                id: 'superAdm',
+                name: 'ADMINISTRADOR DO SISTEMA',
+                rg: '000000000',
+                cpf: '00000000000',
+                password: bcrypt.hashSync('admin', 10),
+                email: 'admin@api.deb.br',
+                birthDate: '2000/01/01',
+                gender: 'M'
+                }
         for (let i = 1; i < amount; i++) {
             var people = getRandomPeople()
             var name = people.fullName
@@ -34,7 +51,7 @@ class LogController {
             }
 
         }
-res.json(data)
+            res.json({amount, users: data})
     }
 
 
